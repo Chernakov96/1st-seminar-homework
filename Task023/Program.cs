7b2f0cfebdf59032a49dc148efe7a41712d96906
Илья Чернаков <ichernakov@2A.local>
2022-09-27 19:04:56 +0300

    task 023 complete

---
+++ Task023/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 23
+// Напишите программу, которая 
+// 1. принимает на вход число (N) и 
+// 2. выдаёт таблицу кубов чисел от 1 до N.
+// Пример:
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void TripleNumber(int num)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        int triple = i * i * i;
+        Console.WriteLine($"{i}  {triple}");
+    }
+}
+TripleNumber(number);
