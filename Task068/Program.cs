36accdef5c2a71d6e06ae19c735a15336ad38feb
Илья Чернаков <ichernakov@2A.local>
2022-10-14 18:11:39 +0300

    task 068 complete

--- Task068/Program.cs
+++ Task068/Program.cs
@@ -3,3 +3,26 @@
 // 2. Даны два неотрицательных числа m и n.
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
+
+Console.WriteLine("Введите неотрицательное число ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите неотрицательное число ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int result = AkkFunc(numberM, numberN);
+Console.WriteLine(result);
+
+
+int AkkFunc(int numM, int numN)
+{
+    if (numM == 0) return numN + 1;
+
+    else if (numM != 0 && numN == 0) return AkkFunc(numM - 1, 1);
+
+    else if (numM > 0 && numN > 0) return AkkFunc(numM - 1, AkkFunc(numM, numN - 1));
+
+    else if(numM < 0 || numN < 0) return 0;
+
+    return AkkFunc(numM, numN);
+}
