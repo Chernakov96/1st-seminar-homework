c09773e8262d8c2fed646c525c26f002092ff8a9
Илья Чернаков <ichernakov@2A.local>
2022-10-14 18:03:49 +0300

    task 066 complete

--- Task066/Program.cs
+++ Task066/Program.cs
@@ -4,3 +4,36 @@
 // Пример:
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите число ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int result = RangeSumElems(numberM, numberN);
+Console.Write(result);
+
+
+
+int RangeSumElems(int numM, int numN)
+{
+    if (numM < numN)
+    {
+        numN = numN + RangeSumElems(numM,numN-1);
+        return numN;
+    }
+
+    if (numM > numN)
+    {
+         numM = numM + RangeSumElems(numM-1,numN);
+         return numM;
+    }
+
+    if (numM == numN)
+    {
+        return numN;
+    }
+    
+    return 0;
+}
